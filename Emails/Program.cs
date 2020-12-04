7c851fc26f9618729f611e61fd6f4254671a7702
Ivan Hristoslavov <hristoslavov.ivanov@gmail.com>
2020-12-04 02:29:56 +0200

    Add

---
+++ Emails/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Extract_Emails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string regexWhitDigits = @"(^|(?<=\s))(([a-zA-Z0-9]+)([\.\-_]?)([A-Za-z0-9]+)(@)([a-zA-Z]+([\.\-_][A-Za-z]+)+))(\b|(?=\s))";
+            string text = Console.ReadLine();
+            Regex regex = new Regex(regexWhitDigits);
+            MatchCollection emails = regex.Matches(text);
+
+            foreach (Match item in emails)
+            {
+                Console.WriteLine(item.Value);
+            }
+        }
+    }
+}
